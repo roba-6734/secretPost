d713a79c94cec63482a3c3630d6903ede55d9a45
roba-6734 <148911183+roba-6734@users.noreply.github.com>
2024-05-21 10:27:37 +0400

    final commit

--- app.js
+++ app.js
@@ -10,13 +10,9 @@ const passportLocalMongoose = require("passport-local-mongoose");
 const GoogleStrategy = require("passport-google-oauth2").Strategy;
 const findOrCreate = require("mongoose-findorcreate");
 
-
-// const _ = require("lodash");
-
-
 const app = express();
 
-app.use(bodyParser.urlencoded({extended: true}));
+app.use(bodyParser.urlencoded({ extended: true }));
 app.use(express.static("public"));
 app.set("view engine", "ejs");
 
@@ -28,24 +24,22 @@ app.use(session({
 
 app.use(passport.initialize());
 app.use(passport.session());
-
-
+//used a  local host
 
 mongoose.connect("mongodb://localhost:27017/userDB");
-
 
 const userSchema = new mongoose.Schema({
     email: String,
     password: String,
     googleId: String,
+    username: String, // Make username optional
+    secret: String
 });
 
 userSchema.plugin(passportLocalMongoose);
 userSchema.plugin(findOrCreate);
 
-
-
-const User = mongoose.model("User",userSchema);
+const User = mongoose.model("User", userSchema);
 
 passport.use(User.createStrategy());
 
@@ -53,8 +47,7 @@ passport.serializeUser(function(user, done) {
     done(null, user.id);
 });
 
-// Deserialize user out of the session
-passport.deserializeUser(async (id, done) => {
+passport.deserializeUser(async function(id, done) {
     try {
         const user = await User.findById(id);
         done(null, user);
@@ -62,102 +55,125 @@ passport.deserializeUser(async (id, done) => {
         done(err);
     }
 });
+
 passport.use(new GoogleStrategy({
-    clientID:     process.env.CLIENT_ID,
+    clientID: process.env.CLIENT_ID,
     clientSecret: process.env.CLIENT_SECRET,
     callbackURL: "http://localhost:3000/auth/google/secrets",
-    passReqToCallback   : true,
+    passReqToCallback: true,
     userProfileURL: "https://www.googleapis.com/oauth2/v3/userinfo"
   },
-  function(request, accessToken, refreshToken, profile, done) {
-    
-    User.findOrCreate({ googleId: profile.id }, function (err, user) {
+  async function(request, accessToken, refreshToken, profile, done) {
+    try {
+      let user = await User.findOne({ googleId: profile.id });
 
-      return done(err, user);
-    });
+      if (!user) {
+        user = new User({
+          googleId: profile.id,
+          username: profile.emails[0].value, // Ensure username is unique or handle it appropriately
+          email: profile.emails[0].value
+        });
+        await user.save();
+      }
+
+      done(null, user);
+    } catch (err) {
+      done(err, null);
+    }
   }
 ));
 
-
-app.get("/",function(req,res){
+app.get("/", function(req, res) {
     res.render("home");
 });
 
-app.get("/login",function(req,res){
+app.get("/login", function(req, res) {
     res.render("login");
+});
 
-});  
-
-app.get("/register",function(req,res){
+app.get("/register", function(req, res) {
     res.render("register");
 });
 
 app.get("/auth/google",
-    passport.authenticate('google', {scope : ["profile"] })
+    passport.authenticate('google', { scope: ["profile", "email"] })
 );
 
-app.get( '/auth/google/secrets',
-    passport.authenticate( 'google', {
+app.get('/auth/google/secrets',
+    passport.authenticate('google', {
         successRedirect: '/secrets',
         failureRedirect: '/login'
-}));
+    })
+);
 
-
-app.get("/secrets",function(req,res){
-    if(req.isAuthenticated()){
-        res.render("secrets");
-        }
-        else{
-            res.redirect("/login");
-            }
+app.get("/secrets", function(req, res) {
+    User.find({"secret" :{$ne: null}}).then(result =>{
+        res.render("secrets", {usersWithSecrets: result});
+        }).catch(err =>{
+            console.log(err);
+    });
 
 });
 
-app.get("/logout",function(req,res){
+app.get("/logout", function(req, res) {
     req.logout(function(err) {
-        if (err) { return next(err); }
+        if (err) {
+            return next(err);
+        }
         res.redirect('/');
-      });
     });
+});
 
+app.get("/submit", function(req,res){
+    if (req.isAuthenticated()) {
+        res.render("submit");
+    } else {
+        res.redirect("/login");
+    }
+})
 
+app.post("/submit",function(req,res){
+    const submittedSecret = req.body.secret;
+        User.findById(req.user.id).then(result=>{
+            result.secret = submittedSecret;
+            result.save();
 
-app.post("/register", function(req,res){
-    User.register({username: req.body.username}, req.body.password, function(err, user){
-        if(err){
+            res.redirect("/secrets");
+
+        });
+
+});
+
+app.post("/register", function(req, res) {
+    User.register({ username: req.body.username }, req.body.password, function(err, user) {
+        if (err) {
             console.log(err);
             res.redirect("/register");
-            }else{
-                passport.authenticate("local")(req,res,function(){
-                    res.redirect("/secrets");
+        } else {
+            passport.authenticate("local")(req, res, function() {
+                res.redirect("/secrets");
             });
-            }
+        }
+    });
+});
 
-    
- })
- });
- 
- app.post("/login",function(req,res){
+app.post("/login", function(req, res) {
     const user = new User({
         username: req.body.username,
         password: req.body.password
-        });
-        req.login(user, function(err){
-            if(err){
-                console.log(err);
-                }else{
-                    passport.authenticate("local")(req,res,function(){  
-                        res.redirect("/secrets");
-                        });
-                        }
+    });
 
-                    });
-   
- });
+    req.login(user, function(err) {
+        if (err) {
+            console.log(err);
+        } else {
+            passport.authenticate("local")(req, res, function() {
+                res.redirect("/secrets");
+            });
+        }
+    });
+});
 
-
-
-
-app.listen(3000,function(){
+app.listen(3000, function() {
     console.log("Server started on port 3000");
 });
